47f54bdcc686b672b2b7d765f549f3f7fa8a5005
daman86999 <52445744+daman86999@users.noreply.github.com>
2023-02-13 14:55:34 +0530

    Server part completed (#1)
    * minor update
    
    * mongose schema added for client and project
    
    * mongoose connection setup added
    
    * client and project query added
    
    * server side completed
    
    * db connections file added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`server started at ${port}`));
