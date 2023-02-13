f9a821a86cb7c82efc1aeb9e3a8ecbc343b21e83
damanpreet <daman.086999@gmail.com>
2023-02-13 13:21:09 +0530

    client and project query added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`server started at ${port}`));
