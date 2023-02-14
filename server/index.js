86873e549d8de525110f97bc0e46eb3beb3fbf11
damanpreet <daman.086999@gmail.com>
2023-02-14 13:58:58 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
@@ -7,6 +8,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
