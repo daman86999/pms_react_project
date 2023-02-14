1233a431ee9e4637771f2e83972cb1cf8034b992
damanpreet <daman.086999@gmail.com>
2023-02-14 14:02:39 +0530

    apollo client initigrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
 import React from "react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./views/Home";
+import Project from "./views/Project";
+import NotFound from "./views/NotFound";
+
+/* A way to merge the data from the cache with the data from the server. */
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(_existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+/* Creating a new ApolloClient instance. */
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    </>
   );
 }
 
