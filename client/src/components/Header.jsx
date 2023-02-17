2da8c8248569fa89493c2d7d07e5dbcde77ecce5
damanpreet <daman.086999@gmail.com>
2023-02-17 17:00:22 +0530

    minor change in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { GiBurningBook } from "react-icons/gi";
+
 export default function Header() {
   return (
     <nav className="navbar bg-light mb-4 p-0">
       <div className="container">
         <a className="navbar-brand" href="/">
-          <div className="d-flex">
-            <span>
-              <GiBurningBook />
-            </span>
-            <div className="ml-2">Project Mgmt</div>
+          <div className="d-flex align-items-center h-20">
+            <div className="ml-2">Zoho Projects</div>
           </div>
         </a>
       </div>
