081631e3949d2f87b257dfbd5052715911fe9c9a
damanpreet <daman.086999@gmail.com>
2023-02-20 14:35:11 +0530

    added auth header to query calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./views/Home";
@@ -10,6 +16,7 @@ import Login from "./views/Login";
 import Signup from "./views/Signup";
 import { useAuth } from "./hooks/useAuth";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useCookies } from "react-cookie";
 
 /* A way to merge the data from the cache with the data from the server. */
 const cache = new InMemoryCache({
@@ -26,14 +33,33 @@ const cache = new InMemoryCache({
   },
 });
 
-/* Creating a new ApolloClient instance. */
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:5000/graphql",
-  cache,
 });
 
 function App() {
   const auth = useAuth();
+
+  const [cookie] = useCookies();
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = cookie.token;
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  /* Creating a new ApolloClient instance. */
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache,
+  });
+
   return (
     <>
       <ApolloProvider client={client}>
