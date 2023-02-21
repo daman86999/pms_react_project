8de5a0ac170e839f6c3b6374fcb5e9081e56fbdc
damanpreet <daman.086999@gmail.com>
2023-02-21 12:06:48 +0530

    docs added

--- client/src/utils/parseJWTtoken.js
+++ client/src/utils/parseJWTtoken.js
@@ -1,3 +1,9 @@
+/**
+ * It takes a JWT token, splits it into three parts, decodes the second part, and returns the decoded
+ * JSON.
+ * @param token - The JWT token that you want to parse.
+ * @returns The JSON payload of the JWT.
+ */
 export function parseJwt(token) {
   var base64Url = token.split(".")[1];
   var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
